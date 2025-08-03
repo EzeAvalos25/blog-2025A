be6f075a7de8954758d277c62b45353aac957d5f
EzeAvalos <ezequiel_avalos_66@hotmail.com>
2025-08-03 12:00:52 -0300

    cambios en users

--- blog/apps/user/signals.py
+++ blog/apps/user/signals.py
@@ -6,89 +6,68 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 @receiver(post_save, sender=User)
-def create_groups_and_permisssions(sender, instance, created, **kawars):
-  if created and instance.is_superuser:
-    try:
-      # TODO: Definir los permisos de POST y de COMMENTS
-      post_content_type = ContentType.objects.get_for_model(Comment)
-      comment_content_type = ContentType.objects.get_for_model(Comment)
-
-      #Permisos de POST
-      viem_post_permission = Permission.objects.get(codename = 'view_post', content_type = post_content_type)
-      add_post_permission = Permission.objects.get(codename = 'add_post', content_type = post_content_type)
-      change_post_permission = Permission.objects.get(codename = 'change_post', content_type = post_content_type)
-      delete_post_permission = Permission.objects.get(codename = 'delete_post', content_type = post_content_type)
-
-      # permisos de COMMENTS
-      view_comments_permission = Permission.objects.get(codename = 'view_comments', content_type = comment_content_type)
-      add_comments_permission = Permission.objects.get(codename = 'add_comments', content_type = comment_content_type)
-      change_comments_permission = Permission.objects.get(codename = 'change_pos', content_type = comment_content_type)
-      delete_comments_permission = Permission.objects.get(codename = 'delete_post', content_type = comment_content_type)
-
-
-      # crear grupos de usuarios registrados
-      registered_group, created = Group.objects.get_or_create(name="Registered")
-      registered_group.permissions.add(
-        viem_post_permission, 
-        view_comments_permission,
-        add_comments_permission,
-        change_comments_permission,
-        delete_comments_permission,
-          # permiso para ver post
-          # permiso para ver comentarios del post
-          # permiso para crear comentarios del post
-          # permiso para actualizar de su comentario en un post
-          # permiso para borrar su comentario en un post
-       )
-
-      # crear grupos de usuarios colaboradores
-      registered_group, created = Group.objects.get_or_create(name="Collaborators")
-      registered_group.permissions.add(
-        viem_post_permission, 
-        add_post_permission,
-        change_post_permission,
-        delete_post_permission,
-        view_comments_permission,
-        add_comments_permission,
-        change_comments_permission,
-        delete_comments_permission,        
-          # permiso para ver post
-          # permiso para crear post
-          # permiso para actualizar su post
-          # permiso para borrar su post
-          # permiso para ver comentarios del post
-          # permiso para crear comentarios del post
-          # permiso para actualizar de su comentario en un post
-          # permiso para borrar su comentario en un post
-       )
-
-
-          # crear grupos de usuarios administradores
-      registered_group, created = Group.objects.get_or_create(name="Admins")
-      registered_group.permissions.add(
-        viem_post_permission, 
-        add_post_permission,
-        change_post_permission,
-        delete_post_permission,
-        view_comments_permission,
-        add_comments_permission,
-        change_comments_permission,
-        delete_comments_permission,
-          # permiso para ver post
-          # permiso para crear post
-          # permiso para actualizar su post
-          # permiso para borrar cualquier post
-          # permiso para ver comentarios del post
-          # permiso para crear comentarios del post
-          # permiso para actualizar de su comentario en un post
-          # permiso para borrar su comentario de cualquier post
-       )
-       
-      print("Grupos y Permisos creados exitosamente")
-    except ContentType.DoesNotExist:
-      print("El tipo aun no se encuentra disponible.")
-    except Permission.DoesNotExist:
-      print("Uno o mas permisos no se encuentran disponibles")
-
-    except:
-      pass
+def create_groups_and_permissions(sender, instance, created, **kawars):
+    if created and instance.is_superuser:
+        try:
+            post_content_type = ContentType.objects.get_for_model(Post)
+            comment_content_type = ContentType.objects.get_for_model(Comment)
+      
+            #Permisos de POST
+            view_post_permission = Permission.objects.get(codename='view_post', content_type=post_content_type)
+            add_post_permission = Permission.objects.get(codename='add_post', content_type=post_content_type)
+            change_post_permission = Permission.objects.get(codename='change_post', content_type=post_content_type)
+            delete_post_permission = Permission.objects.get(codename='delete_post', content_type=post_content_type)
+      
+            # permisos de COMMENTS
+            view_comment_permission = Permission.objects.get(codename='view_comment', content_type=comment_content_type)
+            add_comment_permission = Permission.objects.get(codename='add_comment', content_type=comment_content_type)
+            change_comment_permission = Permission.objects.get(codename= 'change_comment', content_type=comment_content_type)
+            delete_comment_permission = Permission.objects.get(codename='delete_comment', content_type=comment_content_type)
+      
+      
+            # crear grupos de usuarios registrados
+            registered_group, created = Group.objects.get_or_create(name="Registered")
+            registered_group.permissions.add(
+              view_post_permission,        # permiso para ver post
+              view_comment_permission,    # permiso para ver comentarios del post
+              add_comment_permission,     # permiso para crear comentarios del post
+              change_comment_permission,  # permiso para actualizar de su comentario en un post
+              delete_comment_permission,  # permiso para borrar su comentario en un post
+              )
+      
+            # crear grupos de usuarios colaboradores
+            collaborators_group, created = Group.objects.get_or_create(name="Collaborators")
+            collaborators_group.permissions.add(
+              view_post_permission,        # permiso para ver post
+              add_post_permission,         # permiso para crear post
+              change_post_permission,      # permiso para actualizar su post
+              delete_post_permission,      # permiso para borrar su post
+              view_comment_permission,    # permiso para ver comentarios del post
+              add_comment_permission,     # permiso para crear comentarios del post
+              change_comment_permission,  # permiso para actualizar de su comentario en un post
+              delete_comment_permission,  # permiso para borrar su comentario en un post     
+              )
+      
+      
+                # crear grupos de usuarios administradores
+            admins_group, created = Group.objects.get_or_create(name="Admins")
+            admins_group.permissions.add(
+              view_post_permission,        # permiso para ver post 
+              add_post_permission,         # permiso para crear post      
+              change_post_permission,      # permiso para actualizar su post 
+              delete_post_permission,      # permiso para borrar cualquier post 
+              view_comment_permission,    # permiso para ver comentarios del post 
+              add_comment_permission,     # permiso para crear comentarios del post 
+              change_comment_permission,  # permiso para actualizar de su comentario en un post  
+              delete_comment_permission,  # permiso para borrar su comentario de cualquier post   
+                
+             )
+             
+            print("Grupos y Permisos creados exitosamente")
+        except ContentType.DoesNotExist:
+          print("El tipo aun no se encuentra disponible.")
+        except Permission.DoesNotExist:
+          print("Uno o mas permisos no se encuentran disponibles")
+    
+        except:
+          pass
